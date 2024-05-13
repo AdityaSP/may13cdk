ccc542e80dd2f7f43bffd9698bcc22f8c4b89ee0
Aditya <sp.aditya@gmail.com>
2024-05-13 23:53:10 +0530

    With si calculation

--- first.ts
+++ first.ts
@@ -6,3 +6,11 @@ let b :string = "Hi"
 let c :boolean = true
 
 console.log(a, b, c)
+
+// p * t * r /100
+let p = 1000
+let t = 3
+const r = 3.5
+
+const si = p * t * r /100
+console.log("Interest :", si)
