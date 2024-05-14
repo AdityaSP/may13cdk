c99eb4c543b4514be6c572ee791011ecf336c839
Aditya <sp.aditya@gmail.com>
2024-05-14 20:20:42 +0530

    Create classesandobjects.ts

---
+++ classesandobjects.ts
@@ -0,0 +1,36 @@
+
+// Studly Caps AbcdEfgh
+class EmployeeC {
+  fn: string;
+  mn: string;
+  ln: string;
+  title: string;
+  age?: number;
+  empid: number | string;
+    
+    constructor(  fn: string, mn: string, ln: string, title: string, empid: number | string, age? :number){
+        this.fn = fn
+        this.mn = mn
+        this.ln = ln
+        this.title = title
+        this.empid = empid
+        if (typeof age !== undefined)
+            this.age = age
+    }
+
+    // camelCase
+    getBirthYear() {
+        if (typeof this.age !== "undefined") return 2024 - this.age;
+        else return 1980;
+    }
+
+    getFullName() {
+        return this.title + "." + this.fn + " " + this.mn + " " + this.ln;
+    }
+}
+
+const adi = new EmployeeC("Aditya","S","P","Dr",30)
+const jw = new EmployeeC("John","Wick","Wick","Mr","ABC005")
+
+console.log("Birth year of Aditya", adi.getBirthYear())
+console.log("Birth year of JW", jw.getBirthYear());
